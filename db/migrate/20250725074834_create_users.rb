b3ca812bb0b7cafef6505436a8574d2780b2da22
Bryan Perez <bryanprz00@gmail.com>
2025-07-25 02:52:34 -0500

    Add user scaffold

---
+++ db/migrate/20250725074834_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[8.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.date :dob
+      t.integer :prakruti
+      t.integer :vikruti
+
+      t.timestamps
+    end
+  end
+end
