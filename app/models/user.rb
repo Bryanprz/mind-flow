d83476194a7686525bc379027d9c09ac9f7c7612
Bryan Perez <bryanprz00@gmail.com>
2025-07-26 15:05:00 -0500

    Add dosha model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  belongs_to :prakruti, class_name: 'Dosha', optional: true
+  belongs_to :vikruti, class_name: 'Dosha', optional: true
+
+  def name
+    "#{first_name} #{last_name}"
+  end
 end
---
+++ test/factories/doshas.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :dosha do
+    
+  end
+end
