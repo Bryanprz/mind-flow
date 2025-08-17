b682585ba5e3a8938c6f4502ccf285190c23743c
Bryan Perez <bryanprz00@gmail.com>
2025-08-16 21:27:46 -0500

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    // Use absolute paths from Rails root - this works in Docker
-    "./app/views/**/*.html.erb",
-    "./app/helpers/**/*.rb", 
-    "./app/components/**/*.{rb,html.erb}",
-    "./app/assets/stylesheets/**/*.css",
-    "./app/javascript/**/*.js",
-    
-    // Alternative syntax that sometimes works better in Docker
-    "/rails/app/views/**/*.html.erb",
-    "/rails/app/helpers/**/*.rb"
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/assets/stylesheets/**/*.css',
+    './app/components/**/*.{rb,html.erb}',
+    './app/javascript/**/*.js'
   ],
   
-  // Add safelist for problematic utilities
+  // This is the official solution from Rails team
   safelist: [
-    // Spacing utilities that commonly break
+    // Include all common spacing utilities
     {
-      pattern: /^(w|h|p|m|mt|mb|ml|mr|pt|pb|pl|pr|px|py)-\d+$/,
+      pattern: /^(w|h|p|m|mt|mb|ml|mr|pt|pb|pl|pr|px|py|mx|my)-(\d+|auto|full|screen)$/,
     },
-    // Common utilities
-    'w-4', 'h-4', 'p-4', 'm-4', 'mt-20', 'text-sm', 'bg-red-500'
+    // Include all text utilities  
+    {
+      pattern: /^text-(xs|sm|base|lg|xl|2xl|3xl|4xl|5xl|6xl|7xl|8xl|9xl)$/,
+    },
+    // Include all background utilities
+    {
+      pattern: /^bg-(slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(\d+)$/,
+    }
   ]
 }
