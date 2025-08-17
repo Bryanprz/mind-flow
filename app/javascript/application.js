3a62613827db82b3655f7b201de094a273524cf8
Bryan Perez <bryanprz00@gmail.com>
2025-08-17 12:48:14 -0500

    add rails action_text which also adds active storage

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,3 +5,6 @@ import "controllers"
 // Set a cookie with the browser's time zone
 // This will be sent with every subsequent request
 document.cookie = "time_zone=" + Intl.DateTimeFormat().resolvedOptions().timeZone + ";path=/";
+
+import "trix"
+import "@rails/actiontext"
