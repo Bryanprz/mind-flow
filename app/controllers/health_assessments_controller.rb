ae4309b6828568d01990a4773e364e214590ccf3
Bryan Perez <bryanprz00@gmail.com>
2025-08-23 18:42:48 -0500

    .

--- app/controllers/health_assessments_controller.rb
+++ app/controllers/health_assessments_controller.rb
@@ -1,7 +1,7 @@
 class HealthAssessmentsController < ApplicationController
   allow_unauthenticated_access
-  before_action :ensure_current_session_is_resumed, only: %i[start_prakruti_assessment start_vikruti_assessment start_chronic_issues_assessment submit_answers show_results]
-  before_action :set_assessment_entry, only: [:submit_answers, :show_results]
+  before_action :set_health_assessment, only: [:submit_answers]
+  before_action :set_assessment_entry, only: [:show_results]
 
   def ensure_current_session_is_resumed
     resume_session
@@ -21,15 +21,7 @@ class HealthAssessmentsController < ApplicationController
 
   def start_assessment(assessment_type)
     @health_assessment = HealthAssessment.find_by(category: assessment_type)
-
-    # Create a new submission for this assessment
-    @assessment_entry = AssessmentEntry.create!(
-      user: Current.user,  # Will be nil for guests
-      health_assessment: @health_assessment
-    )
-
-    # Store the submission ID in the session
-    session[:assessment_entry_id] = @assessment_entry.id
+    session[:assessment_category] = assessment_type
 
     # Eager load all questions and their options to avoid N+1 queries
     @questions = @health_assessment.assessment_questions.includes(:assessment_options).order(:id)
@@ -40,8 +32,7 @@ class HealthAssessmentsController < ApplicationController
           "main_content_area",
           partial: "health_assessments/question",
           locals: { 
-            questions: @questions,
-            assessment_entry: @assessment_entry
+            questions: @questions
           }
         )
       end
@@ -62,6 +53,14 @@ class HealthAssessmentsController < ApplicationController
               else
                 answers_param.map { |a| a.permit(:question_id, :option_id).to_h.symbolize_keys }
               end
+
+    # Create a new submission for this assessment
+    @assessment_entry = AssessmentEntry.create!(
+      user: Current.user,  # Will be nil for guests
+      health_assessment: @health_assessment
+    )
+    # Store the submission ID in the session
+    session[:assessment_entry_id] = @assessment_entry.id
 
     # Prepare all answers for a single bulk insert
     answers_to_insert = answers.map do |answer|
@@ -90,14 +89,11 @@ class HealthAssessmentsController < ApplicationController
           ),
         ]
       end
-      format.html { redirect_to assessment_results_path(@assessment_entry.id) }
+      format.html { redirect_to assessment_results_path }
     end
   end
 
   def show_results
-    # Find the assessment entry by ID from params
-    @assessment_entry = AssessmentEntry.find(params[:entry_id])
-    
     render "health_assessments/results", locals: {
       assessment_entry: @assessment_entry,
       primary_dosha: @assessment_entry.primary_dosha, 
@@ -108,13 +104,12 @@ class HealthAssessmentsController < ApplicationController
 
   private
 
+  def set_health_assessment
+    assessment_category = session[:assessment_category]
+    @health_assessment = HealthAssessment.find_by(category: assessment_category)
+  end
+
   def set_assessment_entry
     @assessment_entry = AssessmentEntry.find_by(id: session[:assessment_entry_id])
-
-    unless @assessment_entry
-      redirect_to root_path, 
-        alert: "Assessment session expired or invalid. Please start again."
-      return
-    end
   end
 end
