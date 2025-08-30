5edbbeb89e9cf0607406eafaa6771fa6791c7777
Bryan Perez <bryanprz00@gmail.com>
2025-08-30 17:02:51 -0500

    .

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,10 +8,25 @@ if [ ! -f /app/db/production.sqlite3 ]; then
 fi
 
 # Set up the Solid Cable database
-if [ ! -f /app/db/cable.sqlite3 ]; then
+if [ ! -f /app/db/production_cable.sqlite3 ]; then
   echo "Setting up Solid Cable database..."
   RAILS_ENV=production bundle exec rails solid_cable:setup
 fi
+
+# Set up the Solid Cache database
+if [ ! -f /app/db/production_cache.sqlite3 ]; then
+  echo "Setting up Solid Cache database..."
+  RAILS_ENV=production bundle exec rails solid_cache:setup
+fi
+
+# Set up the Solid Queue database
+if [ ! -f /app/db/production_queue.sqlite3 ]; then
+  echo "Setting up Solid Queue database..."
+  RAILS_ENV=production bundle exec rails solid_queue:setup
+fi
+
+# Run migrations for all databases
+RAILS_ENV=production bundle exec rails migrate_all
 
 # Execute the CMD from the Dockerfile
 exec "$@"
