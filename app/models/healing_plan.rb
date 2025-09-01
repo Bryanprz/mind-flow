94fce800173c918e3faa01d189ebc1704fb28825
Bryan Perez <bryanprz00@gmail.com>
2025-09-01 02:22:33 -0500

    add healing plan show

--- app/models/healing_plan.rb
+++ app/models/healing_plan.rb
@@ -2,8 +2,4 @@ class HealingPlan < ApplicationRecord
   belongs_to :user
   belongs_to :healing_plan_template, optional: true # Made optional because existing records might not have a template
   has_many :plan_sections
-
-  def daily_routines
-  end
-  
 end
