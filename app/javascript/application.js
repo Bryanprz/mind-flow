00acef58a52892f1a0aca61787b29141d128e1fa
Bryan Perez <bryan.perez.dev@gmail.com>
2025-09-18 22:06:15 -0500

    update time zone

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,9 +3,16 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "turbo_stream_actions"
 
-// // Set a cookie with the browser's time zone
-// // This will be sent with every subsequent request
-// document.cookie = "time_zone=" + Intl.DateTimeFormat().resolvedOptions().timeZone + ";path=/";
+// Set a cookie with the browser's time zone once, so the server can render dates in the user's zone
+// This will be sent with every subsequent request and used in ApplicationController#set_time_zone
+(() => {
+  const hasTzCookie = document.cookie.match(/(?:^|; )time_zone=/);
+  if (!hasTzCookie) {
+    const tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    // Persist for 1 year
+    document.cookie = `time_zone=${encodeURIComponent(tz)}; path=/; max-age=31536000; SameSite=Lax`;
+  }
+})();
 
 import "trix"
 import "@rails/actiontext"
