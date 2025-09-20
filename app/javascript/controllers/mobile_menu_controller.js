7a58f4139f379789605adbd6af2b5b1c03cd2981
Bryan Perez <bryan.perez.dev@gmail.com>
2025-09-20 02:32:38 -0500

    update mobile screens

---
+++ app/javascript/controllers/mobile_menu_controller.js
@@ -0,0 +1,25 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="mobile-menu"
+export default class extends Controller {
+  static targets = ["menu"]
+
+  connect() {
+    // Close menu when clicking outside
+    document.addEventListener("click", this.handleClickOutside.bind(this))
+  }
+
+  disconnect() {
+    document.removeEventListener("click", this.handleClickOutside)
+  }
+
+  toggle() {
+    this.menuTarget.classList.toggle("hidden")
+  }
+
+  handleClickOutside(event) {
+    if (!this.element.contains(event.target) && !this.menuTarget.classList.contains("hidden")) {
+      this.menuTarget.classList.add("hidden")
+    }
+  }
+}
