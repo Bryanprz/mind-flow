1f631a23d35ce435426bb283106af646cda33ac0
Bryan Perez <bryan.perez.dev@gmail.com>
2025-09-25 00:38:13 -0500

    Add missing solid_queue_recurring_tasks table to SolidQueue migration

--- db/queue_migrate/20250919100000_create_solid_queue_tables.rb
+++ db/queue_migrate/20250919100000_create_solid_queue_tables.rb
@@ -66,5 +66,21 @@ class CreateSolidQueueTables < ActiveRecord::Migration[7.1]
 
       t.index [ :scheduled_at, :priority, :job_id ], name: :idx_sq_scheduled_exec_sched_prio_job
     end
+
+    create_table :solid_queue_recurring_tasks do |t|
+      t.string :key, null: false
+      t.string :schedule, null: false
+      t.string :command, limit: 2048
+      t.string :class_name
+      t.text :arguments
+      t.string :queue_name
+      t.integer :priority, default: 0
+      t.boolean :static, default: true, null: false
+      t.text :description
+      t.timestamps
+
+      t.index [ :key ], name: :idx_sq_recurring_tasks_key, unique: true
+      t.index [ :static ], name: :idx_sq_recurring_tasks_static
+    end
   end
 end
