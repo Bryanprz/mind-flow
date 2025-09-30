ebc46ac0febdf0c0c601f5f63f2a571ad54450fc
Bryan Perez <bryan.perez.dev@gmail.com>
2025-09-30 18:25:13 -0500

    solid cable db fix

--- db/migrate/20250930231759_add_primary_key_to_solid_cable_broadcasts.rb
+++
@@ -1,6 +0,0 @@
-class AddPrimaryKeyToSolidCableBroadcasts < ActiveRecord::Migration[8.0]
-  def change
-    # Add primary key constraint to the id column
-    execute "ALTER TABLE solid_cable_broadcasts ADD CONSTRAINT solid_cable_broadcasts_pkey PRIMARY KEY (id)"
-  end
-end
