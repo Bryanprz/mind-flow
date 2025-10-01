2c6c6ce08a4cc2a3f16ecd2cd38d64db41f38bf9
Bryan Perez <bryan.perez.dev@gmail.com>
2025-10-01 02:18:40 -0500

    solid cable db fix

--- db/cable_schema.rb
+++ db/cable_schema.rb
@@ -1,37 +1,11 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# This file is the source Rails uses to define your schema when running `bin/rails
-# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
-#
-# It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema[8.0].define(version: 2025_10_01_052118) do
+ActiveRecord::Schema[7.1].define(version: 1) do
   create_table "solid_cable_messages", force: :cascade do |t|
-    t.string "channel_class", null: false
-    t.text "channel_parameters"
-    t.text "channel_identifier", null: false
-    t.text "data", null: false
-    t.bigint "stream_id", null: false
-    t.string "stream_name", null: false
+    t.binary "channel", limit: 1024, null: false
+    t.binary "payload", limit: 536870912, null: false
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "channel_hash", null: false
-    t.string "channel", null: false
-    t.text "metadata", default: "{}"
+    t.integer "channel_hash", limit: 8, null: false
+    t.index ["channel"], name: "index_solid_cable_messages_on_channel"
     t.index ["channel_hash"], name: "index_solid_cable_messages_on_channel_hash"
-    t.index ["channel_identifier", "stream_name"], name: "solid_cable_message_compound_primary_key", unique: true
-    t.index ["stream_name"], name: "index_solid_cable_messages_on_stream_name"
-  end
-
-  create_table "solid_cable_processes", force: :cascade do |t|
-    t.string "process_id", null: false
-    t.datetime "last_heartbeat_at", null: false
-    t.datetime "created_at", null: false
-    t.index ["process_id"], name: "index_solid_cable_processes_on_process_id", unique: true
+    t.index ["created_at"], name: "index_solid_cable_messages_on_created_at"
   end
 end
