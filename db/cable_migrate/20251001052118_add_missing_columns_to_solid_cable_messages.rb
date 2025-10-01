686d6cbfbb53c40fcf48de6e97e8693396525eac
Bryan Perez <bryan.perez.dev@gmail.com>
2025-10-01 02:52:40 -0500

    solid cable fix

--- db/cable_migrate/20251001052118_add_missing_columns_to_solid_cable_messages.rb
+++
@@ -1,9 +0,0 @@
-class AddMissingColumnsToSolidCableMessages < ActiveRecord::Migration[8.0]
-  def change
-    add_column :solid_cable_messages, :channel_hash, :string, null: false
-    add_column :solid_cable_messages, :channel, :string, null: false
-    add_column :solid_cable_messages, :metadata, :text, default: '{}'
-    
-    add_index :solid_cable_messages, :channel_hash
-  end
-end
