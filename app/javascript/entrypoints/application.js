4f0b13b6824033f674e37b4acc6081c75732d0c5
Bryan Perez <bryan.perez.dev@gmail.com>
2025-10-15 21:28:44 -0500

    Fix home page scroll

--- app/javascript/entrypoints/application.js
+++ app/javascript/entrypoints/application.js
@@ -11,11 +11,11 @@
 
 // Example: Load Rails libraries in Vite.
 //
-// import * as Turbo from '@hotwired/turbo'
-// Turbo.start()
-//
-// import ActiveStorage from '@rails/activestorage'
-// ActiveStorage.start()
+import * as Turbo from '@hotwired/turbo'
+Turbo.start()
+
+import ActiveStorage from '@rails/activestorage'
+ActiveStorage.start()
 //
 // // Import all channels.
 // const channels = import.meta.globEager('./**/*_channel.js')
